8b9bcf72fe01329759f12d483f7ffb5e684e086e
Ajay Bhargav Baaskaran <ajaybhargavb@gmail.com>
2018-07-17 16:00:19 -0700

    Added some common directives

--- RazorVisualizer/Controllers/ParseController.cs
+++ RazorVisualizer/Controllers/ParseController.cs
@@ -1,8 +1,9 @@
+using System;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Razor.Language;
 using Microsoft.AspNetCore.Razor.Language.Legacy;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
-using System;
 
 namespace RazorVisualizer
 {
@@ -12,10 +13,81 @@ namespace RazorVisualizer
         public IActionResult Parse([FromBody] Source source)
         {
             var document = RazorSourceDocument.Create(source.Content, fileName: null);
-            var parser = new RazorParser();
+            var options = RazorParserOptions.Create(builder => {
+                foreach (var directive in GetDirectives())
+                {
+                    builder.Directives.Add(directive);
+                }
+            });
+            var parser = new RazorParser(options);
             var tree = parser.Parse(document);
             var result = TreeSerializer.Serialize(tree);
             return Content(result);
+        }
+
+        private static IEnumerable<DirectiveDescriptor> GetDirectives()
+        {
+            var directives = new DirectiveDescriptor[]
+            {
+                DirectiveDescriptor.CreateDirective(
+                    "inject",
+                    DirectiveKind.SingleLine,
+                    builder =>
+                    {
+                        builder
+                            .AddTypeToken()
+                            .AddMemberToken();
+
+                        builder.Usage = DirectiveUsage.FileScopedMultipleOccurring;
+                    }),
+                DirectiveDescriptor.CreateDirective(
+                    "model",
+                    DirectiveKind.SingleLine,
+                    builder =>
+                    {
+                        builder.AddTypeToken();
+                        builder.Usage = DirectiveUsage.FileScopedSinglyOccurring;
+                    }),
+                DirectiveDescriptor.CreateDirective(
+                    "namespace",
+                    DirectiveKind.SingleLine,
+                    builder =>
+                    {
+                        builder.AddNamespaceToken();
+                        builder.Usage = DirectiveUsage.FileScopedSinglyOccurring;
+                    }),
+                DirectiveDescriptor.CreateDirective(
+                    "page",
+                    DirectiveKind.SingleLine,
+                    builder =>
+                    {
+                        builder.AddOptionalStringToken();
+                        builder.Usage = DirectiveUsage.FileScopedSinglyOccurring;
+                    }),
+                DirectiveDescriptor.CreateDirective(
+                    SyntaxConstants.CSharp.FunctionsKeyword,
+                    DirectiveKind.CodeBlock,
+                    builder =>
+                    {
+                    }),
+                DirectiveDescriptor.CreateDirective(
+                    SyntaxConstants.CSharp.InheritsKeyword,
+                    DirectiveKind.SingleLine,
+                    builder =>
+                    {
+                        builder.AddTypeToken();
+                        builder.Usage = DirectiveUsage.FileScopedSinglyOccurring;
+                    }),
+                DirectiveDescriptor.CreateDirective(
+                    SyntaxConstants.CSharp.SectionKeyword,
+                    DirectiveKind.RazorBlock,
+                    builder =>
+                    {
+                        builder.AddMemberToken();
+                    }),
+            };
+
+            return directives;
         }
 
         public class Source
